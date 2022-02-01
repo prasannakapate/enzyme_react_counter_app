96f70583de715fd87707d81039a7fd0aaea9db9a
Prasannakumar Kapate <prasanna.1616@gmail.com>
2022-02-01 12:51:10 +0530

    initial test added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import Enzyme, { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new EnzymeAdapter() });
+
+test('renders without error', () => {
+  const wrapper = shallow(<App />);
+  const appComponent = wrapper.find("[data-test='component-app']");
+  expect(appComponent.length).toBe(1);
 });
+
+test('renders increment button', () => {});
+
+test('renders counter display', () => {});
+
+test('counter displays start at 0', () => {});
+
+test('clicking button increments counter', () => {});
